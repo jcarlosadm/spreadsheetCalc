22b2aa779aa12c072f82b8a23afee999b067b9b0
jcarlosadm <carlosmaster10@gmail.com>
2014-11-13 19:55:39 -0300

    adicionados arquivos binary_expression_tree.h e .c

---
+++ binary_expression_tree.c
@@ -0,0 +1,7 @@
+/*
+ * \file binary_expression_tree.c
+ * Arquivo que implementa binary_expression_tree.h
+ */
+
+#include "binary_expression_tree.h"
+
---
+++ binary_expression_tree.h
@@ -0,0 +1,15 @@
+/*
+ * \file binary_expression_tree.h
+ * Arquivo que descreve as funcionalidades de uma árvore de expressão binária
+ */
+
+#ifndef BINARY_EXPRESSION_TREE_H_
+#define BINARY_EXPRESSION_TREE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "stack_double.h"
+
+
+
+#endif /* BINARY_EXPRESSION_TREE_H_ */
