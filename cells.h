45d20484bd8a85c4fc8acba2ee329982fb176607
jcarlosadm <carlosmaster10@gmail.com>
2014-11-14 13:19:01 -0300

    Definido arquivo cells.h

--- cells.h
+++ cells.h
@@ -6,6 +6,14 @@
 #ifndef CELLS_H_
 #define CELLS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "binary_expression_tree.h"
+#include "stack_binExpTree.h"
+#include "functions.h"
+#include "undo_redo_cells.h"
+
 /**
  * Estrutura da matriz de células da planilha
  */
@@ -28,7 +36,49 @@ Cells* CELLS_free(Cells* cells);
 
 /**
  * Obtém expressão de uma célula específica da matriz
+ * \param cells Ponteiro duplo para matriz Cells
+ * \param row Linha da célula
+ * \param column  Coluna da célula
+ * \param expression String a ser preenchida com a expressão da célula
  */
-void CELLS_getExpression(Cells* cells, int row, int column, char *expression);
+void CELLS_getExpression(Cells** cells, int row, int column, char *expression);
+
+/**
+ * Obtém valor da célula
+ * \return Valor da célula
+ * \param cells Ponteiro duplo para matriz Cells
+ * \param row Linha da célula
+ * \param column Coluna da célula
+ */
+double CELLS_getValue(Cells** cells, int row, int column);
+
+/**
+ * Define uma expressão para uma célula específica, calculando o seu valor no processo
+ * \return 1 se obtiver sucesso, e 0 caso contrário
+ * \param cells Ponteiro duplo para matriz Cells
+ * \param row Linha da célula
+ * \param column Coluna da célula
+ * \param expression expressão a ser colocada na célula
+ * \param undoRedo Ponteiro duplo para fila de desfazer/refazer. Informe NULL caso
+ * não deseje guarda a informação na fila de desfazer/refazer
+ */
+int CELLS_setExpression(Cells** cells, int row, int column, const char* expression,
+        UndoRedoCells** undoRedo);
+
+/**
+ * Tenta realizar operação de desfazer na matriz de células
+ * \return 1 se obtiver sucesso e 0 em caso contrário
+ * \param cells Ponteiro duplo para matriz Cells
+ * \param undoRedo Ponteiro duplo para fila de desfazer/refazer
+ */
+int CELLS_undo(Cells** cells, UndoRedoCells** undoRedo);
+
+/**
+ * Tenta realizar operação de refazer na matriz de células
+ * \return 1 se obtiver sucesso e 0 em caso contrário
+ * \param cells Ponteiro duplo para matriz Cells
+ * \param undoRedo Ponteiro duplo para fila de desfazer/refazer
+ */
+int CELLS_redo(Cells** cells, UndoRedoCells** undoRedo);
 
 #endif /* CELLS_H_ */
