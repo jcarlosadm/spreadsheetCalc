bfde8b76a0499dc4fc2f9c2f5c77de53ff070eef
jcarlosadm <carlosmaster10@gmail.com>
2014-11-14 00:01:21 -0300

    implementada stack_binExpTree

--- stack_binExpTree.c
+++ stack_binExpTree.c
@@ -80,6 +80,8 @@ StackBinExpTree* STACKBINEXPTREE_free(StackBinExpTree* stack){
  * \param value Valor a ser adicionado na árvore
  */
 int STACKBINEXPTREE_pushValue(StackBinExpTree** stack, double value){
+    if(!(*stack)) return 0;
+
     BinaryExpTree* newTree = BINARYEXPTREE_create(value);
     if(!newTree) return 0;
 
@@ -97,3 +99,72 @@ int STACKBINEXPTREE_pushValue(StackBinExpTree** stack, double value){
     return 1;
 }
 
+/**
+ * Adiciona uma árvore de expressão binária (que conterá o símbolo fornecido)
+ * na pilha. Essa função retira duas outras árvores do topo e as "junta" abaixo
+ * desta nova, uma à esquerda e outra à direita.
+ * \return 1 em caso de sucesso, 0 em caso de falha
+ * \param stack Ponteiro duplo para StackBinExpTree
+ * \param symbol Símbolo a ser adicionado no nó do topo da árvore a ser adicionada na pilha
+ */
+int STACKBINEXPTREE_pushSymbol(StackBinExpTree** stack, char symbol){
+    if(!(*stack)) return 0;
+
+    if(!(*stack)->top) return 0;
+
+    BinaryExpTree* tree2 = (*stack)->top->tree;
+
+    Element* element = (*stack)->top;
+    (*stack)->top = (*stack)->top->next;
+    free(element);
+
+    if(!(*stack)->top){
+        tree2 = BINARYEXPTREE_free(tree2);
+        return 0;
+    }
+
+    BinaryExpTree* tree1 = (*stack)->top->tree;
+
+    element = (*stack)->top;
+    (*stack)->top = (*stack)->top->next;
+    free(element);
+
+    BinaryExpTree* newTree = BINARYEXPTREE_joinTrees(tree1, tree2, symbol);
+    if(!newTree){
+        tree1 = BINARYEXPTREE_free(tree1);
+        tree2 = BINARYEXPTREE_free(tree2);
+        return 0;
+    }
+
+    Element* newElement = malloc(sizeof(Element));
+    if(!newElement){
+        newTree = BINARYEXPTREE_free(newTree);
+        return 0;
+    }
+
+    newElement->next = (*stack)->top;
+    newElement->tree = newTree;
+
+    (*stack)->top = newElement;
+    return 1;
+}
+
+/**
+ * Calcula expressão da árvore do topo, remove essa árvore do topo e retorna o resultado
+ * \return O resultado da expressão do topo da pilha
+ * \param stack Ponteiro duplo para StackBinExpTree
+ */
+double STACKBINEXPTREE_pop(StackBinExpTree** stack){
+
+    if(!(*stack) || !(*stack)->top) return 0;
+
+    BinaryExpTree* tree = (*stack)->top->tree;
+    Element* element = (*stack)->top;
+    (*stack)->top = (*stack)->top->next;
+    free(element);
+
+    double value = BINARYEXPTREE_evalExpression(tree);
+    tree = BINARYEXPTREE_free(tree);
+
+    return value;
+}
