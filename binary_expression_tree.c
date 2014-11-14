648725ffed3b2bad353d61c92a5cb40d797861df
jcarlosadm <carlosmaster10@gmail.com>
2014-11-13 21:04:55 -0300

    Em binary_expression_tree.h, definidas as funções Em binary_expression_tree.c, definida a estrutura BinaryExpTree e implementadas as funções create e free

--- binary_expression_tree.c
+++ binary_expression_tree.c
@@ -5,3 +5,59 @@
 
 #include "binary_expression_tree.h"
 
+/**********************************************************************
+ * Estruturas
+ **********************************************************************/
+
+/**
+ * Estrutura da árvore de expressão binária
+ */
+struct binaryExpTree{
+    char value;
+    BinaryExpTree* left;
+    BinaryExpTree* right;
+};
+
+/**********************************************************************
+ * Funções privadas
+ **********************************************************************/
+
+
+
+/**********************************************************************
+ * Funções públicas
+ **********************************************************************/
+
+/**
+ * Cria uma árvore com apenas um nó, com filhos nulos
+ * \return Ponteiro para BinaryExpTree
+ * \param value Caractere guardado no único nó da árvore
+ */
+BinaryExpTree* BINARYEXPTREE_create(char value){
+    BinaryExpTree* binaryExpTree = malloc(sizeof(BinaryExpTree));
+    if(!binaryExpTree) return NULL;
+
+    binaryExpTree->value = value;
+    binaryExpTree->left = NULL;
+    binaryExpTree->right = NULL;
+
+    return binaryExpTree;
+}
+
+/**
+ * Libera memória da árvore de expressão binária
+ * \return Retorna NULL
+ * \param binaryExpTree Ponteiro para uma árvore de expressão binária
+ */
+BinaryExpTree* BINARYEXPTREE_free(BinaryExpTree* binaryExpTree){
+    if(!binaryExpTree) return NULL;
+
+    if(binaryExpTree->left)
+        binaryExpTree->left = BINARYEXPTREE_free(binaryExpTree->left);
+    if(binaryExpTree->right)
+        binaryExpTree->right = BINARYEXPTREE_free(binaryExpTree->right);
+
+    free(binaryExpTree);
+    return NULL;
+}
+
--- binary_expression_tree.h
+++ binary_expression_tree.h
@@ -10,6 +10,40 @@
 #include <stdlib.h>
 #include "stack_double.h"
 
+/**
+ * Estrutura da árvore de expressão binária
+ */
+typedef struct binaryExpTree BinaryExpTree;
 
+/**
+ * Cria uma árvore com apenas um nó, com filhos nulos
+ * \return Ponteiro para BinaryExpTree
+ * \param value Caractere guardado no único nó da árvore
+ */
+BinaryExpTree* BINARYEXPTREE_create(char value);
+
+/**
+ * Libera memória da árvore de expressão binária
+ * \return Retorna NULL
+ * \param binaryExpTree Ponteiro para uma árvore de expressão binária
+ */
+BinaryExpTree* BINARYEXPTREE_free(BinaryExpTree* binaryExpTree);
+
+/**
+ * Junta duas árvores de expressão binária sob um nó de valor específico
+ * \return Ponteiro para uma árvore de expressão binária
+ * \param left Ponteiro para a árvore a ser colocada como filho esquerdo da nova árvore
+ * \param right Ponteiro para a árvore a ser colocada como filho direito da nova árvore
+ * \param value Valor do nó pai das duas árvores
+ */
+BinaryExpTree* BINARYEXPTREE_joinTrees(BinaryExpTree* left, BinaryExpTree* right,
+        char value);
+
+/**
+ * Calcula expressão da árvore
+ * \return O resultado da expressão
+ * \param binaryExpTree Ponteiro para a árvore de expressão binária
+ */
+double BINARYEXPTREE_evalExpression(BinaryExpTree* binaryExpTree);
 
 #endif /* BINARY_EXPRESSION_TREE_H_ */
