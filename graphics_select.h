3dd47f641219f178f14903968a46c14ed0b252f9
jcarlosadm <carlosmaster10@gmail.com>
2014-11-19 21:59:29 -0300

    adicionando arquivos graphics_select

---
+++ graphics_select.h
@@ -0,0 +1,21 @@
+/**
+ * \file graphics_select.h
+ * Arquivo que cria um menu de seleção
+ */
+
+#ifndef GRAPHICS_SELECT_H_
+#define GRAPHICS_SELECT_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <unistd.h>
+
+/**
+ * Estrutura de um menu de seleção
+ */
+typedef struct graphicSelect GraphicSelect;
+
+
+
+#endif /* GRAPHICS_SELECT_H_ */
